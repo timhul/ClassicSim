8a5ab37b5545258373bf8bcece70efb6755b6682
Tim Hultman <tim.hultman@gmail.com>
2018-03-18 20:20:26 +0100

    Set IAS as float instead of int.

--- Character/Class/Warrior/Buffs/Flurry.cpp
+++ Character/Class/Warrior/Buffs/Flurry.cpp
@@ -3,11 +3,11 @@
 #include "Warrior.h"
 
 void Flurry::buff_effect_when_applied() {
-    // TODO: Remove assumption player has 5/5 Flurry
-    pchar->increase_ias(30);
+    // TODO: Remove assumption player has 5/5 Flurry, or has Flurry at all
+    pchar->increase_ias(0.3);
 }
 
 void Flurry::buff_effect_when_removed() {
-    // TODO: Remove assumption player has 5/5 Flurry
-    pchar->decrease_ias(30);
+    // TODO: Remove assumption player has 5/5 Flurry, or has Flurry at all
+    pchar->decrease_ias(0.3);
 }
