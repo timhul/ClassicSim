f3463a3846d465495629a6f149119813a12aff77
Tim Hultman <tim.hultman@gmail.com>
2018-03-27 18:37:31 +0200

    Implement non-DPS Fury talents as GenericTalents.

--- Character/Class/Warrior/TalentTrees/Fury.cpp
+++ Character/Class/Warrior/TalentTrees/Fury.cpp
@@ -14,6 +14,7 @@
 #include "ImprovedBerserkerRage.h"
 #include "FlurryTalent.h"
 #include "BloodthirstTalent.h"
+#include "GenericTalent.h"
 
 Fury::Fury(Character *pchar, QObject* parent) :
     TalentTree("Fury", parent)
@@ -29,19 +30,19 @@ Fury::Fury(Character *pchar, QObject* parent) :
     add_talents(tier2);
 
     QMap<QString, Talent*> tier3 {{"3LL", new ImprovedCleave(pchar)},
-                                  {"3ML", new Talent(pchar, "Piercing Howl", "3ML", base_url + "tier3/Spell_shadow_deathscream.png", 1)},
-                                  {"3MR", new Talent(pchar, "Blood Craze", "3MR", base_url + "tier3/Spell_shadow_summonimp.png", 3)},
+                                  {"3ML", new GenericTalent(pchar, "Piercing Howl", "3ML", base_url + "tier3/Spell_shadow_deathscream.png", 1, "", "Requires 10 points in Fury talents", "Causes all enemies near the warrior to be dazed, reducing movement speed by 50% for 6 sec.", 0, 0, false)},
+                                  {"3MR", new GenericTalent(pchar, "Blood Craze", "3MR", base_url + "tier3/Spell_shadow_summonimp.png", 3, "", "Requires 10 points in Fury talents", "Regenerates %1% of your total Health over 6 sec after being the victim of a critical strike.", 1, 1, true)},
                                   {"3RR", new ImprovedBattleShout(pchar)}};
     add_talents(tier3);
 
     QMap<QString, Talent*> tier4 {{"4LL", new DualWieldSpecialization(pchar)},
                                   {"4ML", new ImprovedExecute(pchar)},
-                                  {"4MR", new Talent(pchar, "Enrage", "4MR", base_url + "tier4/Spell_shadow_unholyfrenzy.png", 5)}};
+                                  {"4MR", new GenericTalent(pchar, "Enrage", "4MR", base_url + "tier4/Spell_shadow_unholyfrenzy.png", 5, "", "Requires 15 points in Fury talents", "Gives you a %1% melee damage bonus for 12 sec up to a maximum of 12 swings after being the victim of a critical strike.", 5, 5, true)}};
     add_talents(tier4);
 
     QMap<QString, Talent*> tier5 {{"5LL", new ImprovedSlam(pchar)},
                                   {"5ML", new DeathWish(pchar)},
-                                  {"5RR", new Talent(pchar, "Improved Intercept", "5RR", base_url + "tier5/Ability_rogue_sprint.png", 2)}};
+                                  {"5RR", new GenericTalent(pchar, "Improved Intercept", "5RR", base_url + "tier5/Ability_rogue_sprint.png", 2, "", "Requires 20 points in Fury talents", "Reduces the cooldown of your Intercept ability by %1 sec.", 5, 5, true)}};
     add_talents(tier5);
 
     QMap<QString, Talent*> tier6 {{"6LL", new ImprovedBerserkerRage(pchar)},
@@ -53,6 +54,9 @@ Fury::Fury(Character *pchar, QObject* parent) :
 
     talents["5ML"]->set_bottom_child(talents["7ML"]);
     talents["7ML"]->set_parent(talents["5ML"]);
+
+    talents["4MR"]->set_bottom_child(talents["6MR"]);
+    talents["6MR"]->set_parent(talents["4MR"]);
 }
 
 Fury::~Fury()
