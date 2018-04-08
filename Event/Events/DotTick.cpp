21aa4286e9d00fea314a71c8db7c8c29ce6e3a44
Tim Hultman <tim.hultman@gmail.com>
2018-04-08 12:03:39 +0200

    Add DotTick event.

---
+++ Event/Events/DotTick.cpp
@@ -0,0 +1,13 @@
+
+#include "DotTick.h"
+#include "Spell.h"
+
+DotTick::DotTick(Spell* spell, float priority) {
+    this->spell = spell;
+    this->priority = priority;
+    this->name = "DotTick";
+}
+
+void DotTick::act(void) {
+    spell->perform(0);
+}
---
+++ Event/Events/DotTick.h
@@ -0,0 +1,18 @@
+#ifndef DOTTICK_H
+#define DOTTICK_H
+
+#include "Event.h"
+
+class Spell;
+
+class DotTick: public Event {
+public:
+    DotTick(Spell *spell, float priority);
+
+    virtual void act(void) override;
+protected:
+private:
+    Spell* spell;
+};
+
+#endif // DOTTICK_H
