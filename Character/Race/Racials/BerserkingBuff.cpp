a31731906a89818073460860b3b85de170d205d1
Tim Hultman <tim.hultman@gmail.com>
2018-11-23 10:41:46 +0100

    Remove all non-Warrior-related casts to Warrior.

--- Character/Race/Racials/BerserkingBuff.cpp
+++ Character/Race/Racials/BerserkingBuff.cpp
@@ -1,6 +1,6 @@
 
 #include "BerserkingBuff.h"
-#include "Warrior.h"
+#include "Character.h"
 #include "CharacterStats.h"
 #include "Random.h"
 
@@ -13,9 +13,9 @@ BerserkingBuff::BerserkingBuff(Character* pchar):
 void BerserkingBuff::buff_effect_when_applied() {
     curr_buff = random->get_roll();
     assert(curr_buff >= 10 && curr_buff <= 30);
-    dynamic_cast<Warrior*>(pchar)->increase_attack_speed(static_cast<int>(curr_buff));
+    pchar->increase_attack_speed(static_cast<int>(curr_buff));
 }
 
 void BerserkingBuff::buff_effect_when_removed() {
-    dynamic_cast<Warrior*>(pchar)->decrease_attack_speed(static_cast<int>(curr_buff));
+    pchar->decrease_attack_speed(static_cast<int>(curr_buff));
 }
--- Character/Race/Racials/BloodFuryBuff.cpp
+++ Character/Race/Racials/BloodFuryBuff.cpp
@@ -1,16 +1,16 @@
 
 #include "BloodFuryBuff.h"
-#include "Warrior.h"
+#include "Character.h"
 #include "CharacterStats.h"
 
 void BloodFuryBuff::buff_effect_when_applied() {
     // [MeleeAp] = (base_strength * 2 + base_melee_ap) / 4
     // [102] = (123 * 2 + 160) / 4
-    dynamic_cast<Warrior*>(pchar)->get_stats()->increase_melee_ap(102);
+    pchar->get_stats()->increase_melee_ap(102);
 }
 
 void BloodFuryBuff::buff_effect_when_removed() {
     // [MeleeAp] = (base_strength * 2 + base_melee_ap) / 4
     // [102] = (123 * 2 + 160) / 4
-    dynamic_cast<Warrior*>(pchar)->get_stats()->decrease_melee_ap(102);
+    pchar->get_stats()->decrease_melee_ap(102);
 }
