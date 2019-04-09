6990b598b6c1d74a364842d359e5e719a417d638
Tim Hultman <tim.hultman@gmail.com>
2019-04-09 17:27:35 +0200

    Remove undefined/unused function.

--- Equipment/EquipmentDb/WeaponFileReader.h
+++ Equipment/EquipmentDb/WeaponFileReader.h
@@ -7,14 +7,10 @@ class WeaponFileReader: public ItemFileReader {
 public:
     void weapon_file_handler(QXmlStreamReader &reader, QVector<Item*> &items);
 
-protected:
 private:
-
     void dmg_range_element_reader(const QXmlStreamAttributes &attrs, QMap<QString, QString> &item);
     void create_weapon(QVector<Item*> &items, QMap<QString, QString> &item_map, QVector<QPair<QString, QString>> &stats, QVector<QMap<QString, QString>> &procs);
     int get_weapon_type(const QString &type);
-    QString get_type_string(const QString &slot, const QString &type);
-
 };
 
 #endif // WEAPONFILEREADER_H
