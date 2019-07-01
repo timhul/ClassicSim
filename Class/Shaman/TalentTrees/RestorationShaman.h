8d5f95cabb443e6056c3b4eba5afdc2eb8a95c37
Tim Hultman <tim.hultman@gmail.com>
2019-07-01 13:32:38 +0200

    Fix cppcheck error in Restoration: allocation not stored.
    It is seemingly a false positive in cppcheck 1.87 as there are no actual
    memory management errors, and the reporting is inconsistent when
    allocating new TalentStatIncrease instances.
    
    The fix requires reworking the publicly available allocation functions
    for all TalentTree children.

--- Class/Shaman/TalentTrees/RestorationShaman.h
+++ Class/Shaman/TalentTrees/RestorationShaman.h
@@ -10,13 +10,12 @@ public:
     RestorationShaman(Shaman* shaman);
     ~RestorationShaman() override = default;
 
-    Talent* get_natures_guidance();
-    Talent* get_tidal_mastery();
-
 private:
     Shaman* shaman;
     ShamanSpells* spells;
 
-    Talent* get_ancestral_healing();
-    Talent* get_healing_way();
+    void add_natures_guidance(QMap<QString, Talent*>& talent_tier);
+    void add_tidal_mastery(QMap<QString, Talent*>& talent_tier);
+    void add_ancestral_healing(QMap<QString, Talent*>& talent_tier);
+    void add_healing_way(QMap<QString, Talent*>& talent_tier);
 };
