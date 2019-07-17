c678f62a7769d0d2e3f5584320ff3e81473f62f4
Tim Hultman <tim.hultman@gmail.com>
2019-07-17 09:11:56 +0200

    Improve accuracy of Berserking buff.

--- Character/Race/Racials/BerserkingBuff.cpp
+++ Character/Race/Racials/BerserkingBuff.cpp
@@ -6,7 +6,7 @@
 
 BerserkingBuff::BerserkingBuff(Character* pchar):
     SelfBuff(pchar, "Berserking", "Assets/races/Racial_troll_berserk.png", 10, 0),
-    random(new Random(10, 43)),
+    random(new Random(0, 30)),
     curr_buff(0)
 {}
 
@@ -15,12 +15,24 @@ BerserkingBuff::~BerserkingBuff() {
 }
 
 void BerserkingBuff::buff_effect_when_applied() {
+    pchar->increase_melee_attack_speed(10);
+    pchar->increase_ranged_attack_speed(10);
+
     curr_buff = random->get_roll();
+    if (curr_buff == 0)
+        return;
+
     pchar->increase_melee_attack_speed(curr_buff);
     pchar->increase_ranged_attack_speed(curr_buff);
 }
 
 void BerserkingBuff::buff_effect_when_removed() {
+    pchar->decrease_melee_attack_speed(10);
+    pchar->decrease_ranged_attack_speed(10);
+
+    if (curr_buff == 0)
+        return;
+
     pchar->decrease_melee_attack_speed(curr_buff);
     pchar->decrease_ranged_attack_speed(curr_buff);
 }
