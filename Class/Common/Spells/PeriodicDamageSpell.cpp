timhul/ClassicSim

commit 188681c41fdf0e0dcdfa3b2613b23cab89dd8605

Refactor periodic spells.
In order to formalize and unify the handling of e.g. target debuffs
falling off before a dot tick and other such problems that has to be
considered for every debuff the periodic buffs are now a base class
SpellPeriodic.

Add a SpellCallback event for handling special cases. For now only Seal
of Command uses it and before formalizing it further the new
SpellCallback event only works with Seal of Command.

Improve resource and execution time statistics for periodic effects.
This is simpler now that it only has to be solved once.

Remove ResourceGain for the moment.

Class/Common/Spells/PeriodicDamageSpell.cpp
@@ -1,0 +1,68 @@
+#include "PeriodicDamageSpell.h"
+
+#include "Character.h"
+#include "CharacterStats.h"
+#include "MagicSchools.h"
+#include "NoEffectUniqueDebuff.h"
+
+PeriodicDamageSpell::PeriodicDamageSpell(const QString& name,
+                                         const QString& icon,
+                                         Character* pchar,
+                                         const RestrictedByGcd restricted_by_gcd,
+                                         const MagicSchool school,
+                                         const double tick_rate,
+                                         const int duration,
+                                         const unsigned full_duration_dmg,
+                                         const unsigned resource_cost,
+                                         const double cast_time_ms,
+                                         const double spell_coefficient)
+    :
+      SpellPeriodic(name, icon, pchar,
+                    new NoEffectUniqueDebuff(pchar, duration, name, icon, Hidden::No),
+                    restricted_by_gcd,
+                    ResourceType::Mana,
+                    tick_rate,
+                    0,
+                    0),
+      school(school),
+      full_duration_dmg(full_duration_dmg),
+      duration(duration),
+      spell_coefficient(spell_coefficient),
+      num_ticks(duration / tick_rate),
+      reported_resource_cost(resource_cost / num_ticks),
+      reported_execution_time(cast_time_ms / num_ticks / 1000)
+{}
+
+PeriodicDamageSpell::~PeriodicDamageSpell() {
+    delete marker_buff;
+}
+
+double PeriodicDamageSpell::get_spell_coefficient_from_duration(const double duration) {
+    return duration / 15;
+}
+
+bool PeriodicDamageSpell::check_application_success() {
+    return true;
+}
+
+void PeriodicDamageSpell::new_application_effect() {
+
+}
+
+void PeriodicDamageSpell::refresh_effect() {
+
+}
+
+void PeriodicDamageSpell::tick_effect() {
+    const double spell_power_bonus = pchar->get_stats()->get_spell_damage(school) * spell_coefficient;
+    const double damage_dealt = (full_duration_dmg + spell_power_bonus) * pchar->get_stats()->get_magic_school_damage_mod(school) / num_ticks + tick_rest;
+    tick_rest += damage_dealt - round(damage_dealt);
+
+    add_hit_dmg(static_cast<int>(round(damage_dealt)), reported_resource_cost, reported_execution_time);
+
+    add_next_tick();
+}
+
+void PeriodicDamageSpell::reset_effect() {
+    tick_rest = 0;
+}

Class/Common/Spells/PeriodicResourceGainSpell.cpp
@@ -1,44 +1,41 @@
 #include "PeriodicResourceGainSpell.h"
 
-#include <utility>
-
 #include "Character.h"
 #include "ClassStatistics.h"
-#include "CooldownControl.h"
-#include "Engine.h"
-#include "ResourceGain.h"
+#include "NoEffectSelfBuff.h"
 #include "StatisticsResource.h"
 
 PeriodicResourceGainSpell::PeriodicResourceGainSpell(const QString& name,
                                                      const QString& icon,
                                                      Character* pchar,
-                                                     bool restricted_by_gcd,
-                                                     double cooldown,
-                                                     double tick_rate,
-                                                     double tick_until,
+                                                     const RestrictedByGcd restricted_by_gcd,
+                                                     const double tick_rate,
+                                                     const int duration,
                                                      QVector<QPair<ResourceType, unsigned>> resource_gains)
     :
-      Spell(name, icon, pchar, new CooldownControl(pchar, cooldown), restricted_by_gcd, ResourceType::Rage, 0),
-      tick_rate(tick_rate),
-      tick_until(tick_until),
+      SpellPeriodic(name, icon, pchar,
+                    new NoEffectSelfBuff(pchar, duration, name, icon, Hidden::No),
+                    restricted_by_gcd,
+                    ResourceType::Mana,
+                    tick_rate,
+                    0,
+                    0),
       resource_gains(std::move(resource_gains))
 {}
 
 PeriodicResourceGainSpell::~PeriodicResourceGainSpell() {
-    delete cooldown;
+    delete marker_buff;
 }
 
-void PeriodicResourceGainSpell::spell_effect() {
-    double next_tick = engine->get_current_priority() + tick_rate;
+void PeriodicResourceGainSpell::new_application_effect() {
 
-    if (next_tick > tick_until)
-        return;
-
-    auto* new_event = new ResourceGain(this, next_tick);
-    this->engine->add_event(new_event);
 }
 
-void PeriodicResourceGainSpell::perform_periodic() {
+void PeriodicResourceGainSpell::refresh_effect() {
+
+}
+
+void PeriodicResourceGainSpell::tick_effect() {
     for (const auto & gain : resource_gains) {
         unsigned before_gain;
         unsigned delta = 0;
@@ -66,9 +63,17 @@ void PeriodicResourceGainSpell::perform_periodic() {
             statistics_resource->add_resource_gain(gain.first, delta);
     }
 
-    spell_effect();
+    add_next_tick();
+}
+
+void PeriodicResourceGainSpell::reset_effect() {
+
 }
 
 void PeriodicResourceGainSpell::prepare_set_of_combat_iterations_spell_specific() {
     this->statistics_resource = pchar->get_statistics()->get_resource_statistics(name, icon);
+}
+
+bool PeriodicResourceGainSpell::check_application_success() {
+    return true;
 }

Class/Common/Buffs/EssenceOfTheRed.cpp
@@ -3,14 +3,15 @@
 #include "Character.h"
 #include "CharacterStats.h"
 #include "PeriodicResourceGainSpell.h"
-#include "ResourceGain.h"
 
 EssenceOfTheRed::EssenceOfTheRed(Character* pchar):
     SelfBuff(pchar, "Essence of the Red", "Assets/spell/Spell_fire_lavaspawn.png", 180, 0),
     spell(new PeriodicResourceGainSpell(name,
                                         "Assets/spell/Spell_fire_lavaspawn.png",
                                         pchar,
-                                        false, 0.0, 1.0, duration,
+                                        RestrictedByGcd::No,
+                                        1.0,
+                                        duration,
                                         QVector<QPair<ResourceType, unsigned>>({{ResourceType::Mana, 500},
                                                                                 {ResourceType::Rage, 20},
                                                                                 {ResourceType::Energy, 50}
@@ -27,6 +28,7 @@ void EssenceOfTheRed::buff_effect_when_applied() {
 }
 
 void EssenceOfTheRed::buff_effect_when_removed() {
+
 }
 
 void EssenceOfTheRed::prepare_set_of_combat_iterations_spell_specific() {

Class/Common/Buffs/NoEffectSelfBuff.h
@@ -7,11 +7,11 @@ class Proc;
 class NoEffectSelfBuff: public SelfBuff {
 public:
     NoEffectSelfBuff(Character* pchar,
-                 const int duration,
-                 const QString& name = "NO_NAME",
-                 const QString& icon = NO_ICON,
-                 const bool hidden = Hidden::Yes,
-                 const int charges = 0);
+                     const int duration,
+                     const QString& name = "NO_NAME",
+                     const QString& icon = NO_ICON,
+                     const bool hidden = Hidden::Yes,
+                     const int charges = 0);
 
     void link_buff_expiration(Buff* buff_to_cancel);
 

Class/Common/Buffs/NoEffectUniqueDebuff.h
@@ -7,11 +7,11 @@ class Proc;
 class NoEffectUniqueDebuff: public UniqueDebuff {
 public:
     NoEffectUniqueDebuff(Character* pchar,
-                 const int duration,
-                 const QString& name = "NO_NAME",
-                 const QString& icon = NO_ICON,
-                 const bool hidden = Hidden::Yes,
-                 const int charges = 0);
+                         const int duration,
+                         const QString& name = "NO_NAME",
+                         const QString& icon = NO_ICON,
+                         const bool hidden = Hidden::Yes,
+                         const int charges = 0);
 
     void link_buff_expiration(Buff* buff_to_cancel);
 

Class/Common/Spells/FireballInstant.cpp
@@ -8,6 +8,7 @@
 #include "DotTick.h"
 #include "Engine.h"
 #include "NoEffectUniqueDebuff.h"
+#include "PeriodicDamageSpell.h"
 #include "Random.h"
 #include "Utils/Check.h"
 
@@ -17,7 +18,10 @@ FireballInstant::FireballInstant(Character* pchar,
                                  const unsigned instant_max,
                                  const unsigned dmg_over_duration,
                                  const int duration,
+                                 const unsigned resource_cost,
+                                 const unsigned casting_time,
                                  const double spell_coefficient,
+                                 const double spell_coefficient_dot,
                                  const int spell_rank) :
     Spell(QString("Fireball%1").arg(name), "Assets/spell/Spell_fire_flamebolt.png", pchar,
           new CooldownControl(pchar, 0.0),
@@ -25,21 +29,16 @@ FireballInstant::FireballInstant(Character* pchar,
           ResourceType::Mana,
           0,
           spell_rank),
-    fireball_dot(new NoEffectUniqueDebuff(pchar, duration, QString("Fireball%1 (rank %2)").arg(name).arg(spell_rank), "Assets/spell/Spell_fire_flamebolt.png", Hidden::No)),
+    fireball_dot(new PeriodicDamageSpell(QString("Fireball DoT (rank %1)").arg(spell_rank),
+                                         "Assets/spell/Spell_fire_flamebolt.png", pchar, RestrictedByGcd::No,
+                                         MagicSchool::Fire, 2.0, duration, dmg_over_duration, resource_cost, casting_time, spell_coefficient_dot)),
     instant_dmg(new Random(instant_min, instant_max)),
-    max_ticks(static_cast<unsigned>(round(static_cast<double>(duration) / 2.0))),
-    base_dmg_per_tick(static_cast<double>(dmg_over_duration) / max_ticks),
     spell_coefficient(spell_coefficient)
 {
-    check((max_ticks > 0), "FireballInstant max ticks is zero");
-
-    fireball_dot->enable_buff();
+    fireball_dot->enable();
 }
 
 FireballInstant::~FireballInstant() {
-    if (fireball_dot->is_enabled())
-        fireball_dot->disable_buff();
-
     delete fireball_dot;
     delete cooldown;
     delete instant_dmg;
@@ -58,7 +57,7 @@ void FireballInstant::spell_effect() {
         return increment_full_resist();
     }
 
-    apply_fireball_dot();
+    fireball_dot->perform();
 
     const unsigned damage_dealt = instant_dmg->get_roll() + static_cast<unsigned>(round(pchar->get_stats()->get_spell_damage(MagicSchool::Fire) * spell_coefficient));
     const double resist_mod = get_partial_resist_dmg_modifier(resist_roll);
@@ -78,33 +77,6 @@ void FireballInstant::spell_effect() {
     }
 }
 
-void FireballInstant::perform_periodic() {
-    if (!fireball_dot->is_active())
-        return reset_effect();
-
-    const double damage_dealt = (base_dmg_per_tick + damage_remaining) * pchar->get_stats()->get_magic_school_damage_mod(MagicSchool::Fire);
-    damage_remaining = std::max(0.0, damage_dealt - round(damage_dealt));
-
-    add_hit_dmg(static_cast<int>(round(damage_dealt)), 0, 0);
-
-    --num_ticks_left;
-    if (num_ticks_left > 0)
-        this->engine->add_event(new DotTick(this, engine->get_current_priority() + 2.0));
-}
-
-void FireballInstant::apply_fireball_dot() {
-    fireball_dot->apply_buff();
-
-    if (!fireball_dot->is_active())
-        return reset_effect();
-
-    if (num_ticks_left == 0)
-        this->engine->add_event(new DotTick(this, engine->get_current_priority() + 2.0));
-
-    num_ticks_left = max_ticks;
-}
-
-void FireballInstant::reset_effect() {
-    damage_remaining = 0;
-    num_ticks_left = 0;
+void FireballInstant::prepare_set_of_combat_iterations_spell_specific() {
+    fireball_dot->prepare_set_of_combat_iterations();
 }

Class/Common/Spells/FireballInstant.h
@@ -4,6 +4,7 @@
 
 class Buff;
 class Random;
+class PeriodicDamageSpell;
 
 class FireballInstant: public Spell {
 public:
@@ -13,28 +14,24 @@ public:
                     const unsigned instant_max,
                     const unsigned dmg_over_duration,
                     const int duration,
-                    const double spell_coefficient = 0.0,
-                    const int spell_rank = 1);
+                    const unsigned resource_cost,
+                    const unsigned casting_time,
+                    const double spell_coefficient,
+                    const double spell_coefficient_dot,
+                    const int spell_rank);
     ~FireballInstant() override;
 
 private:
     friend class Fireball;
 
-    Buff* fireball_dot;
+    PeriodicDamageSpell* fireball_dot;
     Random* instant_dmg;
-    double damage_remaining {0};
-    const double period_tick {2.0};
-    const unsigned max_ticks;
-    const double base_dmg_per_tick;
-    unsigned num_ticks_left {0};
     const double spell_coefficient;
     unsigned crit_bonus {0};
     double last_damage_dealt;
 
     void spell_effect() override;
-    void reset_effect() override;
-    void perform_periodic() override;
-    void apply_fireball_dot();
+    void prepare_set_of_combat_iterations_spell_specific() override;
 
     int magic_attack_result {MagicAttackResult::MISS};
 };

Class/Common/Spells/PeriodicDamageSpell.h
@@ -1,0 +1,44 @@
+#pragma once
+
+#include "SpellPeriodic.h"
+
+#include <QVector>
+
+class StatisticsResource;
+
+enum class MagicSchool: int;
+
+class PeriodicDamageSpell : public SpellPeriodic {
+public:
+    PeriodicDamageSpell(const QString& name,
+                        const QString& icon,
+                        Character* pchar,
+                        const RestrictedByGcd restricted_by_gcd,
+                        const MagicSchool school,
+                        const double tick_rate,
+                        const int duration,
+                        const unsigned full_duration_dmg,
+                        const unsigned resource_cost,
+                        const double cast_time_ms,
+                        const double spell_coefficient);
+    ~PeriodicDamageSpell() override;
+
+    static double get_spell_coefficient_from_duration(const double duration);
+
+private:
+    const MagicSchool school;
+    const unsigned full_duration_dmg;
+    const double duration;
+    const double spell_coefficient;
+    const double num_ticks;
+    const double reported_resource_cost;
+    const double reported_execution_time;
+
+    double tick_rest {0.0};
+
+    bool check_application_success() override;
+    void new_application_effect() override;
+    void refresh_effect() override;
+    void tick_effect() override;
+    void reset_effect() override;
+};

commit 7280cee6a9cab4d0aba982128bb2e1066c3b767e

Disable marker buff if enabled upon destruction.
This fixes a crash when using Sulfuras that was introduced in
188681c41fdf0e0dcdfa3b2613b23cab89dd8605, when disabling Fireball marker
buff was removed from FireballInstant.

The same behavior is changed in PeriodicResourceGainSpell, although this
is to be considered a likely dormant bug as no actual crash has been
reported.

Class/Common/Spells/PeriodicDamageSpell.cpp
@@ -37,6 +37,9 @@ PeriodicDamageSpell::PeriodicDamageSpell(const QString& name,
 {}
 
 PeriodicDamageSpell::~PeriodicDamageSpell() {
+    if (marker_buff->is_enabled())
+        marker_buff->disable_buff();
+
     delete marker_buff;
 }
 

Class/Common/Spells/PeriodicResourceGainSpell.cpp
@@ -24,6 +24,9 @@ PeriodicResourceGainSpell::PeriodicResourceGainSpell(const QString& name,
 {}
 
 PeriodicResourceGainSpell::~PeriodicResourceGainSpell() {
+    if (marker_buff->is_enabled())
+        marker_buff->disable_buff();
+
     delete marker_buff;
 }
 