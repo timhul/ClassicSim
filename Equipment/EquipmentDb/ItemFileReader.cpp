timhul/ClassicSim

commit 7a43cc4a456acb287b859646fbac7514b6102a21

Add possibility to specify proc source in <proc> element.

Equipment/EquipmentDb/ItemFileReader.cpp
@@ -131,10 +131,10 @@ void ItemFileReader::stats_element_reader(QXmlStreamReader &reader, QVector<QPai
 }
 
 void ItemFileReader::proc_element_reader(QXmlStreamReader &reader, QVector<QMap<QString, QString>>& procs) {
+    QMap<QString, QString> map;
     while (reader.readNextStartElement()) {
         QXmlStreamAttributes attrs = reader.attributes();
         if (reader.name() == "spell") {
-            QMap<QString, QString> map;
             add_mandatory_attr(attrs, "name", map);
             add_mandatory_attr(attrs, "rate", map);
             add_mandatory_attr(attrs, "internal_cd", map);
@@ -149,10 +149,20 @@ void ItemFileReader::proc_element_reader(QXmlStreamReader &reader, QVector<QMap<
             add_attr(attrs, "value", map);
             add_attr(attrs, "type", map);
             add_attr(attrs, "spell_dmg_coefficient", map);
-            procs.append(map);
+        }
+        else if(reader.name() == "source") {
+            add_attr(attrs, "proc_magic_hit", map);
+            add_attr(attrs, "proc_melee_auto", map);
+            add_attr(attrs, "proc_melee_skill", map);
+            add_attr(attrs, "proc_melee_weapon_side", map);
+            add_attr(attrs, "proc_ranged_auto", map);
+            add_attr(attrs, "proc_ranged_skill", map);
         }
         reader.skipCurrentElement();
     }
+
+    if (!map.empty())
+        procs.append(map);
 }
 
 void ItemFileReader::use_element_reader(QXmlStreamReader &reader, QVector<QMap<QString, QString>>& uses) {

Equipment/EquipmentDb/Weapons/2hSwords.xml
@@ -120,6 +120,7 @@
 		<proc>
 			<!-- TODO: Find actual proc rate. -->
 			<spell name="GENERIC_STAT_BUFF" type="STRENGTH" amount="300" instant="yes" rate="0.05" duration="8" internal_cd="0"/>
+			<source proc_melee_weapon_side="true"/>
 		</proc>
 		<special_equip_effect>
 		Chance on hit: Increases Strength by 300 for 8 sec.

Equipment/Item/Item.cpp
@@ -470,10 +470,10 @@ void Item::set_procs(const int eq_slot) {
                                        proc_rate, buff);
         }
         else if (proc_name == "GENERIC_STAT_BUFF") {
-            add_default_proc_sources(proc_sources, eq_slot);
+            add_proc_sources_from_map(proc_sources, i, eq_slot);
             Buff* buff = new GenericStatBuff(pchar, name, icon,
                                              i["duration"].toInt(),
-                                             {{get_valid_item_stat(i["type"]), static_cast<unsigned>(amount)}});
+                                             {{get_item_stats_from_string(i["type"]), static_cast<unsigned>(amount)}});
             proc = new GenericBuffProc(pchar, name, icon, proc_sources, proc_rate, buff);
         }
         else if (proc_name == "INSTANT_FIREBALL") {
@@ -510,6 +510,43 @@ void Item::add_default_proc_sources(QVector<ProcInfo::Source>& proc_sources, con
     }
 }
 
+void Item::add_proc_sources_from_map(QVector<ProcInfo::Source>& proc_sources, const QMap<QString, QString>& proc_map, const int eq_slot) {
+    if (proc_map.contains("proc_magic_hit"))
+        proc_sources.append(ProcInfo::Source::MagicSpell);
+
+    if (proc_map.contains("proc_magic_hit"))
+        proc_sources.append(ProcInfo::Source::MagicSpell);
+
+    if (proc_map.contains("proc_ranged_auto"))
+        proc_sources.append(ProcInfo::Source::RangedAutoShot);
+
+    if (proc_map.contains("proc_ranged_skill"))
+        proc_sources.append(ProcInfo::Source::RangedSpell);
+
+    if (proc_map.contains("proc_melee_auto")) {
+        proc_sources.append(ProcInfo::Source::MainhandSwing);
+        proc_sources.append(ProcInfo::Source::OffhandSwing);
+    }
+
+    if (proc_map.contains("proc_melee_skill"))
+        proc_sources.append(ProcInfo::Source::MainhandSpell);
+
+    if (proc_map.contains("proc_melee_weapon_side")) {
+        switch (eq_slot) {
+        case EquipmentSlot::MAINHAND:
+            proc_sources.append(ProcInfo::Source::MainhandSpell);
+            proc_sources.append(ProcInfo::Source::MainhandSwing);
+            break;
+        case EquipmentSlot::OFFHAND:
+            proc_sources.append(ProcInfo::Source::OffhandSwing);
+            break;
+        default:
+            qDebug() << "Could not add proc source for" << name;
+            break;
+        }
+    }
+}
+
 MagicSchool Item::get_magic_school(const QString& name) {
     if (name.startsWith("PHYSICAL_"))
         return MagicSchool::Physical;
@@ -530,22 +567,6 @@ MagicSchool Item::get_magic_school(const QString& name) {
     return MagicSchool::Physical;
 }
 
-ItemStats Item::get_valid_item_stat(const QString& item_stat) const {
-    if (item_stat == "STRENGTH")
-        return ItemStats::Strength;
-    if (item_stat == "AGILITY")
-        return ItemStats::Agility;
-    if (item_stat == "INTELLECT")
-        return ItemStats::Intellect;
-    if (item_stat == "STAMINA")
-        return ItemStats::Stamina;
-    if (item_stat == "SPIRIT")
-        return ItemStats::Spirit;
-
-    check(false, QString("Unknown item stat '%1'").arg(item_stat).toStdString());
-    return ItemStats::Armor;
-}
-
 ItemStats Item::get_item_stats_from_string(const QString& item_stat) const {
     if (item_stat == "ATTACK_SPEED")
         return ItemStats::AttackSpeedPercent;
@@ -559,6 +580,14 @@ ItemStats Item::get_item_stats_from_string(const QString& item_stat) const {
         return ItemStats::AttackPower;
     else if (item_stat == "STRENGTH")
         return ItemStats::Strength;
+    else if (item_stat == "AGILITY")
+        return ItemStats::Agility;
+    else if (item_stat == "INTELLECT")
+        return ItemStats::Intellect;
+    else if (item_stat == "STAMINA")
+        return ItemStats::Stamina;
+    else if (item_stat == "SPIRIT")
+        return ItemStats::Spirit;
     else if (item_stat == "SPELL_DAMAGE")
         return ItemStats::SpellDamage;
     else if (item_stat == "MANA_PER_5")

Equipment/Item/Item.h
@@ -105,7 +105,7 @@ protected:
     QString get_tooltip(const QVector<QString>&) const;
 
     void add_default_proc_sources(QVector<ProcInfo::Source>& proc_sources, const int eq_slot);
+    void add_proc_sources_from_map(QVector<ProcInfo::Source>& proc_sources, const QMap<QString, QString>& proc_map, const int eq_slot);
     MagicSchool get_magic_school(const QString& name);
-    ItemStats get_valid_item_stat(const QString& item_stat) const;
     ItemStats get_item_stats_from_string(const QString& item_stat) const;
 };

commit be1206baee31319ab4845d0a9045977a3d267edb

Remove <source> sub-element from <proc> element.
Incorporate its data as an attribute in the <spell> element instead.

This fixes a bug where if multiple <spell> elements are defined in the
<proc> element only the last defined was propagated. Note that this only
affects Thunderfury as of this commit, effectively fixing a bug that was
introduced in 7a43cc4a456acb287b859646fbac7514b6102a21 where its 100%
nature damage proc was not propagated properly to its constructed Item.

Equipment/EquipmentDb/ItemFileReader.cpp
@@ -136,9 +136,10 @@ void ItemFileReader::stats_element_reader(QXmlStreamReader &reader, QVector<QPai
 }
 
 void ItemFileReader::proc_element_reader(QXmlStreamReader &reader, QVector<QMap<QString, QString>>& procs) {
-    QMap<QString, QString> map;
     while (reader.readNextStartElement()) {
+        QMap<QString, QString> map;
         QXmlStreamAttributes attrs = reader.attributes();
+
         if (reader.name() == "spell") {
             add_mandatory_attr(attrs, "name", map);
             add_mandatory_attr(attrs, "rate", map);
@@ -154,8 +155,6 @@ void ItemFileReader::proc_element_reader(QXmlStreamReader &reader, QVector<QMap<
             add_attr(attrs, "value", map);
             add_attr(attrs, "type", map);
             add_attr(attrs, "spell_dmg_coefficient", map);
-        }
-        else if(reader.name() == "source") {
             add_attr(attrs, "proc_magic_hit", map);
             add_attr(attrs, "proc_melee_auto", map);
             add_attr(attrs, "proc_melee_skill", map);
@@ -163,11 +162,12 @@ void ItemFileReader::proc_element_reader(QXmlStreamReader &reader, QVector<QMap<
             add_attr(attrs, "proc_ranged_auto", map);
             add_attr(attrs, "proc_ranged_skill", map);
         }
+
+        if (!map.empty())
+            procs.append(map);
+
         reader.skipCurrentElement();
     }
-
-    if (!map.empty())
-        procs.append(map);
 }
 
 void ItemFileReader::use_element_reader(QXmlStreamReader &reader, QVector<QMap<QString, QString>>& uses) {

Equipment/EquipmentDb/Weapons/1hFists.xml
@@ -19,8 +19,7 @@
 			<stat type="AGILITY" value="4"/>
 		</stats>
 		<proc>
-			<spell name="GENERIC_STAT_BUFF" type="ATTACK_SPEED" amount="30" instant="yes" rate="0.025" duration="5" internal_cd="0"/>
-			<source proc_melee_weapon_side="true"/>
+			<spell name="GENERIC_STAT_BUFF" type="ATTACK_SPEED" amount="30" instant="yes" rate="0.025" duration="5" internal_cd="0" proc_melee_weapon_side="true"/>
 		</proc>
 		<special_equip_effect>
 			Chance on hit: Increases your attack speed by 30% for 5 sec.

Equipment/EquipmentDb/Weapons/1hMaces.xml
@@ -122,8 +122,7 @@
 		<info name="Empyrean Demolisher" type="MACE" slot="MH" unique="no" req_lvl="60" item_lvl="66" quality="EPIC" boe="no" icon="Inv_hammer_05.png"/>
 		<dmg_range min="94" max="175" speed="2.8"/>
 		<proc>
-			<spell name="GENERIC_STAT_BUFF" type="ATTACK_SPEED" amount="20" instant="yes" rate="0.07" duration="10" internal_cd="0"/>
-			<source proc_melee_weapon_side="true"/>
+			<spell name="GENERIC_STAT_BUFF" type="ATTACK_SPEED" amount="20" instant="yes" rate="0.07" duration="10" internal_cd="0" proc_melee_weapon_side="true"/>
 		</proc>
 		<special_equip_effect>
 			Chance on hit: Increases your attack speed by 20% for 10 sec.

Equipment/EquipmentDb/Weapons/1hSwords.xml
@@ -44,8 +44,7 @@
 		<info name="Argent Avenger" type="SWORD" slot="1H" unique="no" req_lvl="55" item_lvl="62" quality="RARE" boe="no" icon="Inv_sword_05.png"/>
 		<dmg_range min="71" max="108" speed="2.2"/>
 		<proc>
-			<spell name="GENERIC_STAT_BUFF" type="ATTACK_POWER_UNDEAD" amount="200" instant="yes" rate="0.041666677" duration="10" internal_cd="0"/>
-			<source proc_melee_weapon_side="true"/>
+			<spell name="GENERIC_STAT_BUFF" type="ATTACK_POWER_UNDEAD" amount="200" instant="yes" rate="0.041666677" duration="10" internal_cd="0" proc_melee_weapon_side="true"/>
 		</proc>
 		<special_equip_effect>
 			Chance on hit: Increases Attack Power against Undead by 200 for 10 sec.
@@ -98,8 +97,7 @@
 		<info name="Lord General's Sword" type="SWORD" slot="MH" unique="no" req_lvl="51" item_lvl="56" quality="RARE" boe="no" icon="Inv_sword_25.png"/>
 		<dmg_range min="67" max="125" speed="2.6"/>
 		<proc>
-			<spell name="GENERIC_STAT_BUFF" type="ATTACK_POWER" amount="50" instant="yes" rate="0.08" duration="30" internal_cd="0"/>
-			<source proc_melee_weapon_side="true"/>
+			<spell name="GENERIC_STAT_BUFF" type="ATTACK_POWER" amount="50" instant="yes" rate="0.08" duration="30" internal_cd="0" proc_melee_weapon_side="true"/>
 		</proc>
 		<special_equip_effect>
 			Chance on hit: Increases attack power by 50 for 30 sec.

Equipment/EquipmentDb/Weapons/2hSwords.xml
@@ -131,8 +131,7 @@
 			<stat type="STAMINA" value="16"/>
 		</stats>
 		<proc>
-			<spell name="GENERIC_STAT_BUFF" type="STRENGTH" amount="300" instant="yes" rate="0.056666667" duration="8" internal_cd="0"/>
-			<source proc_melee_weapon_side="true"/>
+			<spell name="GENERIC_STAT_BUFF" type="STRENGTH" amount="300" instant="yes" rate="0.056666667" duration="8" internal_cd="0" proc_melee_weapon_side="true"/>
 		</proc>
 		<special_equip_effect>
 		Chance on hit: Increases Strength by 300 for 8 sec.

Equipment/EquipmentDb/Weapons/Staves.xml
@@ -456,8 +456,7 @@
 			<stat type="SPELL_DAMAGE" value="56"/>
 		</stats>
 		<proc>
-			<spell name="GENERIC_STAT_BUFF" type="SPELL_PENETRATION" amount="50" instant="yes" rate="0.05" duration="8" internal_cd="0"/>
-			<source proc_magic_hit="true"/>
+			<spell name="GENERIC_STAT_BUFF" type="SPELL_PENETRATION" amount="50" instant="yes" rate="0.05" duration="8" internal_cd="0" proc_magic_hit="true"/>
 		</proc>
 		<source>
 		Quest reward. Requires expensive materials and boss drop from Temple of Ahn'Qiraj (AQ40).