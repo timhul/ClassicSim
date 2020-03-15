98910a63ae7d716b1d4b9e1a2f6156bfbebfaa29
jokal2 <56339632+jokal2@users.noreply.github.com>
2020-03-14 21:09:31 -0400

    Fix berserker/battle stance threat modifiers (#184)

--- Class/Warrior/Buffs/BattleStanceBuff.cpp
+++ Class/Warrior/Buffs/BattleStanceBuff.cpp
@@ -8,9 +8,9 @@ BattleStanceBuff::BattleStanceBuff(Warrior* warrior) : SelfBuff(warrior, "Battle
 }
 
 void BattleStanceBuff::buff_effect_when_applied() {
-    warrior->get_stats()->increase_total_threat_mod(-10);
+    warrior->get_stats()->increase_total_threat_mod(-20);
 }
 
 void BattleStanceBuff::buff_effect_when_removed() {
-    warrior->get_stats()->decrease_total_threat_mod(-10);
+    warrior->get_stats()->decrease_total_threat_mod(-20);
 }
